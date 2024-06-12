2a13f64ec684b4b67a9308b4f6ae9133bddc0b73
TawfeeqRifat <tawfeeqrifat@gmail.com>
2024-06-12 12:58:21 +0530

    Tic Got No Tact

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:ticgotnotact/pages/mainMenu.dart';
+import 'package:flutter_launcher_icons/abs/icon_generator.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/logger.dart';
+import 'package:flutter_launcher_icons/macos/macos_icon_generator.dart';
+import 'package:flutter_launcher_icons/macos/macos_icon_template.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/pubspec_parser.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/web/web_icon_generator.dart';
+import 'package:flutter_launcher_icons/web/web_template.dart';
+import 'package:flutter_launcher_icons/windows/windows_icon_generator.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
+import 'pages/GamePage.dart';
+
+void main() {
+  runApp(const TicGotNoTact());
+}
+
+class TicGotNoTact extends StatelessWidget {
+  const TicGotNoTact({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tic Got No Tac',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black12,
+      ),
+      routes: {
+        '/MainMenu': (context ) => const MainMenu(),
+        '/GameWindow': (context ) => const GameWindow()
+      },
+      home: const MainMenu(),
+    );
+  }
+}
+
---
+++ lib/pages/mainMenu.dart
@@ -0,0 +1,174 @@
+import 'dart:io';
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:newton_particles/newton_particles.dart';
+
+import '../Animations/custom.dart';
+
+Color backGround=Colors.white.withOpacity(0.95);
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({super.key});
+
+  @override
+  State<MainMenu> createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backGround,
+      body: Newton(
+                activeEffects: [
+                  RainEffect(
+                    effectConfiguration: const EffectConfiguration(
+                      foreground: false,
+                      //fadeInCurve: Curves.easeOut,
+                      particlesPerEmit:1,
+                      distanceCurve: Curves.easeIn,
+                      minBeginScale: 1,
+                      maxBeginScale: 10,
+                      minDuration: 1000,
+                      maxDuration: 5000,
+                    ),
+                    particleConfiguration: ParticleConfiguration(
+                      shape: CircleShape(),
+                      size: const Size(10,10),
+                      color: const SingleParticleColor(color: Colors.yellow),
+
+                    )
+                  )
+                ],
+
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Center(
+                                    child: SeeThroughText(
+                                      text: 'TIC GOT NO TACT',
+                                      fontSize: 90,
+                                      opacity: 0.9,
+                                    )
+                                  ),
+                                  const SizedBox(height: 100,),
+
+                                  //play button
+                                  Padding(padding: const EdgeInsets.only(left: 20),
+                                    child: GestureDetector(
+                                      onTap: () {
+                                        Navigator.pushNamed(
+                                            context, '/GameWindow');
+                                      },
+                                      child: CustomPaint(
+                                        painter: customShape(),
+                                        child: const SizedBox(
+                                          height: 100,
+                                          child: Center(
+                                            child: Row(
+                                              mainAxisAlignment: MainAxisAlignment.end,
+                                              children: [
+                                                SeeThroughText(
+                                                  text: 'PLAY',
+                                                  fontSize: 60.0,
+                                                  opacity: 0.7,
+                                                ),
+                                                SizedBox(width: 40,),
+                                              ],
+                                            )
+                                          )
+                                        ),
+                                      ),
+                                    )
+                                  ),
+                                  const SizedBox(height:30,),
+
+                                  //exit button
+                                  Padding(padding: EdgeInsets.only(right: 20),
+                                      child: GestureDetector(
+                                        onTap: ()=> SystemNavigator.pop(),
+                                        child: Transform.flip(
+                                          flipX: true,
+                                          child: CustomPaint(
+                                            painter: customShape(),
+                                            child: SizedBox(
+                                                height: 100,
+                                                child: Center(
+                                                    child: Row(
+                                                      mainAxisAlignment: MainAxisAlignment.end,
+                                                      children: [
+                                                        Transform.flip(
+                                                          flipX: true,
+                                                          child: const SeeThroughText(
+                                                            text: 'EXIT',
+                                                            fontSize: 60.0,
+                                                            opacity: 0.7,
+                                                          ),
+                                                        ),
+                                                        const SizedBox(width: 40,),
+                                                      ],
+                                                    )
+                                                )
+                                            ),
+                                          ),
+                                        ),
+                                      )
+                                  ),
+
+                                ],
+                              ),
+      ),
+
+    );
+  }
+}
+
+class SeeThroughText extends StatelessWidget {
+  const SeeThroughText({super.key,required this.text,this.opacity,required this.fontSize});
+  final String text;
+  final double? opacity;
+  final double fontSize;
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: opacity?? 0.9,
+      child: Stack(
+          children: [
+            Text(text,
+              style: GoogleFonts.signikaNegative(
+                  fontWeight: FontWeight.bold,
+                  fontSize: fontSize,
+                  color: backGround,
+                  shadows: [
+                    const Shadow( // bottomLeft
+                      offset: Offset(-1.5, -1.5),
+                      color: Colors.black,
+                    ),
+                    const Shadow( // bottomRight
+                        offset: Offset(1.5, -1.5),
+                        color: Colors.black
+                    ),
+                    const Shadow( // topRight
+                        offset: Offset(1.5, 1.5),
+                        color: Colors.black
+                    ),
+                    const Shadow( // topLeft
+                      offset: Offset(-1.5, 1.5),
+                      color: Colors.black,
+                    ),
+                  ]
+              ),
+              textAlign: TextAlign.center,
+
+            ),
+          ]
+      ),
+    );
+  }
+}
